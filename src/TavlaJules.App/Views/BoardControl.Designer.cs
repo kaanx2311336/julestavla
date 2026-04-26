d1af0c25fe405f3b2000833bfc6ec89bed693032
Claw Cloud User <user@example.com>
2026-04-26 16:33:24 +0300

    Auto-apply Jules session 12854917924274159508

---
+++ src/TavlaJules.App/Views/BoardControl.Designer.cs
@@ -0,0 +1,21 @@
+namespace TavlaJules.App.Views;
+
+partial class BoardControl
+{
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    }
+}
